256aa6ef5fe3f549e5ae033e74f5d916962729ad
mrhelmus <mrhelmus@gmail.com>
2014-07-31 17:30:43 -0400

    AICc for phylolm modified from AICcmodavg

---
+++ AICc.r
@@ -0,0 +1,11 @@
+##phylolm objects
+AICc.phylolm
+<-function(mod, return.K = FALSE, second.ord = TRUE, nobs = NULL, ...){
+
+    if(identical(nobs, NULL)) {n <- length(mod$residuals)} else {n <- nobs}
+    LL <- logLik(mod)$logLik
+    K <- logLik(mod)$df  #extract correct number of parameters included in model - this includes LM
+    if(second.ord == TRUE) {AICc <- -2*LL+2*K*(n/(n-K-1))}  else{AICc <- -2*LL+2*K}
+    if(return.K == TRUE) AICc[1] <- K #attributes the first element of AICc to K
+    return(AICc)
+  }
