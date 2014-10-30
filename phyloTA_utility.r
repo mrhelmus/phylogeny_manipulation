d5072902a999ed44c024e65e017be7532f75d4a7
mrhelmus <mrhelmus@gmail.com>
2014-10-29 20:02:07 -0400

    added multi match function

--- phyloTA_utility.r
+++ phyloTA_utility.r
@@ -1,4 +1,4 @@
-#' \code{comparative.comm} creates a community comparative ecology object
+#' \code{getSpp} parses a fasta file to return species names
 #'
 #' @param x a fasta read cluster from PhyloTA with define as taxon label
 #' @details gives taxon labels, species names from a fasta object with annotations as given by phyloTA with 'Use define as taxon label'
@@ -10,7 +10,6 @@
 #' }
 
 getSpp<-function(x){
-          require(seqinr)
           ans<-unlist(getAnnot(x))
           ans<-strsplit(ans,"[|]")
           hj<-function(x){x[3]}
@@ -18,5 +17,20 @@ getSpp<-function(x){
           ans<-strsplit(ans," ")
           hj<-function(x){paste(x[1:2],collapse=" ")}
           ans<-unlist(lapply(ans,hj))
+          ans
 }
 
+#' \code{mti.match} returns multiple matches
+#'
+#' @param x a fasta read cluster from PhyloTA with define as taxon label
+#' @details gives taxon labels, species names from a fasta object with annotations as given by phyloTA with 'Use define as taxon label'
+#' @return a character vector
+#' @examples \dontrun{
+#' data(daphnia)
+#' x <- daphnia@fasta
+#' getSpp(x)
+#' }
+
+mti.match<-function(x,table){
+  which(!is.na(match(table,x)))
+}
