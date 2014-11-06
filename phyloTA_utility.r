c99b6bfdb5c459017a6b53ce70f5877652035094
mrhelmus <mrhelmus@gmail.com>
2014-11-06 09:05:05 -0500

    returns genus names

--- phyloTA_utility.r
+++ phyloTA_utility.r
@@ -20,6 +20,29 @@ getSpp<-function(x){
           ans
 }
 
+#' \code{getGen} parses a fasta file to return genus names
+#'
+#' @param x a fasta read cluster from PhyloTA with define as taxon label
+#' @details gives taxon labels, species names from a fasta object with annotations as given by phyloTA with 'Use define as taxon label'
+#' @return a character vector
+#' @examples \dontrun{
+#' data(daphnia)
+#' x <- daphnia@fasta
+#' getGen(x)
+#' }
+
+getGen<-function(x){
+          ans<-unlist(getAnnot(x))
+          ans<-strsplit(ans,"[|]")
+          hj<-function(x){x[3]}
+          ans<-unlist(lapply(ans,hj))
+          ans<-strsplit(ans," ")
+          hj<-function(x){x[1]}
+          ans<-unlist(lapply(ans,hj))
+          ans
+}
+
+
 #' \code{mti.match} returns multiple matches
 #'
 #' @param x a fasta read cluster from PhyloTA with define as taxon label
