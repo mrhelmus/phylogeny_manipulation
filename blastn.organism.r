8c1e6f2f2b82b4b28d55095aaa9ac0d3868bc5cb
mrhelmus <mrhelmus@gmail.com>
2014-11-15 15:00:35 -0500

    filter turned off

--- blastn.organism.r
+++ blastn.organism.r
@@ -5,7 +5,7 @@
 #' @param organism name of NCBI clades in which to search for BLAST hits
 #' @param database NCBI database to use, default \code{nr}
 #' @param hitListSize number of hits to keep
-#' @param filter sequence filter; \code{L} for Low Complexity, \code{R} for Human Repeats, \code{m} for Mask lookup
+#' @param filter sequence filter; \code{NULL} is no filter, \code{L} for Low Complexity, \code{R} for Human Repeats, \code{m} for Mask lookup
 #' @param expect the BLAST expect value above which matches will be returned
 #' @param attempts the number of times to query the NCBI server until an stop error is trigered
 #' @param snooze length of time, in seconds, to wait before another query attempt is attempted
@@ -24,17 +24,24 @@
 #'   blastn.organism(myseq, organism="Daphnia", database = "nr", hitListSize = "10", filter = "L", expect = "1e-10", attempts = 10, snooze=5)
 #' }
 blastn.organism <- function (x, organism="Animalia", database = "nr", hitListSize = "10",
-                        filter = "L", expect = "1e-10", attempts = 10, snooze=5)
+                        filter = NULL, expect = "1e-10", attempts = 10, snooze=5)
 {
   program <- "blastn"
   baseUrl <- "http://www.ncbi.nlm.nih.gov/blast/Blast.cgi"
+  if(is.null){
+  query <- paste("QUERY=", as.character(x), "&DATABASE=", database,
+                 "&HITLIST_SIZE=", hitListSize, "&EXPECT=", expect, "&PROGRAM=", program,
+                 "&ENTREZ_QUERY=", organism, sep = "")
+  } else {
   query <- paste("QUERY=", as.character(x), "&DATABASE=", database,
                  "&HITLIST_SIZE=", hitListSize, "&FILTER=", filter, "&EXPECT=",
                  expect, "&PROGRAM=", program,
                  "&ENTREZ_QUERY=", organism, sep = "")
+  }
+  
   url0 <- sprintf("%s?%s&CMD=Put", baseUrl, query)
   results <- tempfile()
-  Sys.sleep(snooze)
+  #Sys.sleep(snooze)
   require(XML)
   post <- htmlTreeParse(url0, useInternalNodes = TRUE)
   X <- post[["string(//comment()[contains(., \"QBlastInfoBegin\")])"]]
@@ -64,7 +71,7 @@ blastn.organism <- function (x, organism="Animalia", database = "nr", hitListSiz
   hspev <- xpathApply(result, "//Hsp_evalue", xmlValue)
   if(length(hspev)==0){
     res<-c(NA,NA,NA,NA,NA)
-    names(res)<-c("id","definition","hitnum","evalue")
+    names(res)<-c("id","definition","hitlength","hitnum","evalue")
     return(data.frame(t(res)))
   } else {
     hitid <- xpathApply(result, "//Hit_id", xmlValue)
@@ -80,7 +87,7 @@ blastn.organism <- function (x, organism="Animalia", database = "nr", hitListSiz
         res<-rbind(res,c(hitid[kk],hitdef[kk],hitlength[kk],hspnum[k],hspev[k]))
       } else {res<-rbind(res,c(hitid[kk],hitdef[kk],hitlength[kk],hspnum[k],hspev[k]))}
     }
-    colnames(res)<-c("id","definition","hitnum","evalue")
+    colnames(res)<-c("id","definition","hitlength","hitnum","evalue")
     return(data.frame(res))
   }
 }
